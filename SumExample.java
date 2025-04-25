507711479f3b7655a743adadeeeabae9508f8359
Ojashwee07 <166322643+Ojashwee07@users.noreply.github.com>
2025-04-25 10:50:19 +0530

    method

---
+++ SumExample.java
@@ -0,0 +1,9 @@
+public class SumExample {
+    static void sum(int a, int b) {
+        int result = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + result);
+    }
+    public static void main(String[] args) {
+        sum(5, 10);
+    }
+}
